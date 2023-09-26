4eb7997c437d595d9dff775c30f34e82a1437bec
Gabriel <adv.gabinho099@gmail.com>
2023-09-25 22:58:59 -0300

    criando HOME e começando com ABOUT

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray('.panel').forEach((panel, i) =>{
+    ScrollTrigger.create({
+        trigger: panel,
+        start: "top top",
+        pin: true,
+        pinSpacing: false,
+    })
+})
+
+
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
