9e38b2c3a6a70a0acdcb9dd8b34fc1e56f53163e
Gabriel <adv.gabinho099@gmail.com>
2023-10-24 23:54:54 -0300

    section home responsiva

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,29 @@
-
+//////////////////// HOME IMG //////////////////
 new hoverEffect({
-  parent: document.querySelector('.home-img'),
-  intensity: 0.5,  
-  image1: 'assets/imgs/profile.jpeg',
-    image2: 'assets/imgs/profile.jpeg',
+    parent: document.querySelector('.home-img'),
+    intensity: 0.5,  
+    speedIn: 2,
+    speedOut: 2,
+    image1: 'assets/imgs/home-img.jpg',
+    image2: 'assets/imgs/home-hover.jpg',
     displacementImage: 'assets/imgs/4.png'
+})
+
+
+
+
+
+
+////////////////////// LENIS SCROLL /////////////////
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
 
 })
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
