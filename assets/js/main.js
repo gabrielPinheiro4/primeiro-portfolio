8fa2444e2c5131bb9785097ea3b70e269f3fdaa5
Gabriel <adv.gabinho099@gmail.com>
2023-10-25 22:07:46 -0300

    section projetos

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,25 @@ new hoverEffect({
     speedIn: 2,
     speedOut: 2,
     image1: 'assets/imgs/home-img.jpg',
-    image2: 'assets/imgs/home-hover.jpg',
+    image2: 'assets/imgs/home-img.jpg',
     displacementImage: 'assets/imgs/4.png'
 })
 
 
 
+///////////// PROJETOS HORIZONTAL SCROLL ////////////
+const projetos = document.querySelectorAll('.projeto');
 
+gsap.to(projetos, {
+  xPercent: -100 * (projetos.length - 1),
+  ease: 'none',
+  scrollTrigger: {
+    trigger: '.projetos-link',
+    pin: true,
+    scrub: true,
+    end: () => '+=' + document.querySelector('.projetos-link').offsetWidth
+  }
+})
 
 
 ////////////////////// LENIS SCROLL /////////////////
